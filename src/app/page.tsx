8fae0b6bca929c488191c42b4ef4e4306b88a5cb
Diego Restrepo <alejorvela@gmail.com>
2025-10-01 14:16:24 -0500

    feat: adjust global

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect } from 'react'
 import { useRouter } from 'next/navigation'
-import "@assets/styles/global.css";
+import "assets/styles/global.css";
 
 export default function HomePage() {
   const router = useRouter()
