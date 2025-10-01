f198b1b07f067f93322b3423ad7f4927474f01de
Diego Restrepo <alejorvela@gmail.com>
2025-10-01 16:31:32 -0500

    feat: adjust config files

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,34 @@
-import path from 'path';
+import path from "path";
 
 const nextConfig = {
   distDir: "./dist",
+  experimental: {
+    optimizePackageImports: ["i18next", "react-i18next"],
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+
+  eslint: {
+    dirs: ["src"],
+    ignoreDuringBuilds: true,
+  },
+  sassOptions: {
+    includePaths: ["./src"],
+  },
   webpack: (config) => {
     config.resolve.alias = {
       ...config.resolve.alias,
-      '@': path.resolve('./src'),
-      '@/app': path.resolve('./src/app'),
-      '@assets': path.resolve('./public/assets'),
-      '@/assets': path.resolve('./public/assets'),
-      '@public': path.resolve('./public'),
-      '@components': path.resolve('./src/components'),
-      '@pages': path.resolve('./src/app'),
-      '@services': path.resolve('./src/services'),
-      '@utils': path.resolve('./src/utils'),
-      '@tests': path.resolve('./tests'),
+      "@": path.resolve("./src"),
+      "@/app": path.resolve("./src/app"),
+      "@assets": path.resolve("./public/assets"),
+      "@/assets": path.resolve("./public/assets"),
+      "@public": path.resolve("./public"),
+      "@components": path.resolve("./src/components"),
+      "@pages": path.resolve("./src/app"),
+      "@services": path.resolve("./src/services"),
+      "@utils": path.resolve("./src/utils"),
+      "@tests": path.resolve("./tests"),
     };
     return config;
   },
